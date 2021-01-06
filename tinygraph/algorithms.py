5a762b7f867e44b3de97dd83b05c23ae5dedbdab
WilliamsJakeL <williamsjl@uchicago.edu>
2021-01-06 15:01:46 -0600

    Moved shortest paths to fastutils

--- tinygraph/algorithms.py
+++ tinygraph/algorithms.py
@@ -5,50 +5,7 @@ import tinygraph.fastutils
 from queue import Queue
 
 from tinygraph.fastutils import get_connected_components
-# from tinygraph.fastutils import get_shortest_paths
-
-@profile
-def get_shortest_paths(tg, weighted):
-    """
-    Get the distance from each node to each other node on the shortest path. 
-    Uses Floyd-Warshall to calculate the distances of the shortest paths.
-
-    Inputs:
-        tg (TinyGraph): The graph to find the shortest paths in.
-
-    Outputs:
-        distances ([[int]]): A list of the distance to each node. The lists are
-            ordered by node number, so distances[0] is a list of the distances 
-            from node 0 to the other nodes (e.g. distances[0][3] = distance from
-            node 0 to node 3 shortest path from 0 to 3; distances[2][2] = 0 is 
-            distance from node 2 to itself). If no path exists between the 
-            nodes, the result is None.
-        weighted (bool): Whether to consider the weights of the edges, or to 
-            consider only the lengths of the path. If weighted is true, the 
-            distance of a path is calculated by the sum of the weights on the 
-            path. If false, the distance is calculated by the number of nodes on
-            the path.
-    """
-    distances = [[0 if i == j else None for i in range(tg.node_N)]\
-                                        for j in range(tg.node_N)]
-    for e1, e2, w in tg.edges(weight=True):
-        if weighted:
-            distances[e1][e2] = w 
-            distances[e2][e1] = w
-        else:
-            distances[e1][e2] = 1 
-            distances[e2][e1] = 1
-    for k in range(tg.node_N):
-        for j in range(tg.node_N):
-            for i in range(tg.node_N):
-                if not distances[i][k] is None and not distances[k][j] is None:
-                    newL = distances[i][k] + distances[k][j]
-                    if distances[i][j] is None or distances[i][j] > newL:
-                        distances[i][j] = newL
-    for i in range(tg.node_N):
-        if distances[i][i] < 0:
-            raise Exception("Graph has a negative cycle.")
-    return distances
+from tinygraph.fastutils import get_shortest_paths
 
 def get_min_cycles(tg):
     """
